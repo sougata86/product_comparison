6f477651ed0379d8283573141c3ed94a56709ac7
sougata86 <roysougata1986@gmail.com>
2020-01-17 19:51:55 +0530

    production value set to false

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -11,7 +11,7 @@
 // };
 
 export const environment = {
-  production: true,
+  production: false,
   elasticSearchUrl: 'http://111.93.162.238:9200/',
   searchIndex: 'new-test-comparison',
   searchType: 'new-test-comparison',
